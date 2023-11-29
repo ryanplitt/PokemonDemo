ed4a6891bd4c6573c0debbaafe3347cd50e440c0
Ryan Plitt <ryan.plitt@icloud.com>
2023-11-29 05:55:19 -0700

    Starting Project

---
+++ PokemonTestingDemo/ModelController.swift
@@ -0,0 +1,8 @@
+//
+//  ModelController.swift
+//  PokemonTestingDemo
+//
+//  Created by Ryan Plitt on 11/28/23.
+//
+
+import Foundation
--- PokemonTestingDemo/ViewController.swift
+++ PokemonTestingDemo/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var pokemonLabel: UILabel!
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pokemonLabel.text = ""
     }
 
 
+    @IBAction func goButtonTapped(_ sender: Any) {
+        numberTextField.resignFirstResponder()
+        guard let id = Int(numberTextField.text!) else {
+            return print("There was an error getting a number")
+        }
+        pokemonLabel.text = ""
+        pokemonImageView.image = nil
+        activityIndicator.startAnimating()
+        // Do the work to fetch pokemon here
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        goButtonTapped(self)
+        return true
+    }
 }
 
